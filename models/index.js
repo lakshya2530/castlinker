5da67a1f91ee37c438b3f2f288ad43ec63c48085
lakshya2530 <112060854+sharmalakshya077@users.noreply.github.com>
2025-04-29 16:01:59 +0530

    updated code

--- models/index.js
+++ models/index.js
@@ -7,6 +7,18 @@ require('dotenv').config();
 // const sequelize = new Sequelize(process.env.DB_URI);
 
 // Option 2: Separate DB config
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//   }
+  
+// );
+
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -14,6 +26,9 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    define: {
+      schema: 'public', // 👈 This is important
+    },
   }
 );
 
