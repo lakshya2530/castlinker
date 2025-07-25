25cff9d41264f585d3a2b82f4dfb6e143fdc0e8d
lakshya2530 <112060854+sharmalakshya077@users.noreply.github.com>
2025-07-25 14:12:19 +0530

    updated code

--- models/Milestone.js
+++ models/Milestone.js
@@ -5,6 +5,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       project_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
